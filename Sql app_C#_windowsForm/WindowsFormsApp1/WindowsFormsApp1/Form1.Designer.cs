b008b2bde2c6ebf7b64576fa16c439e5ca76297e
chaitanya <chaituchaitanya456@gmail.com>
2018-07-24 10:15:48 -0500

    Application to demonstrate CRUD DB operations using Connectors and Windows forms

---
+++ Sql app_C#_windowsForm/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Assign3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MySqlStatementTextBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.MySqlStatementLabel = new System.Windows.Forms.Label();
+            this.ExecuteButton = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.ListBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.departmentLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MySqlStatementTextBox
+            // 
+            this.MySqlStatementTextBox.Location = new System.Drawing.Point(105, 56);
+            this.MySqlStatementTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.MySqlStatementTextBox.Name = "MySqlStatementTextBox";
+            this.MySqlStatementTextBox.Size = new System.Drawing.Size(221, 20);
+            this.MySqlStatementTextBox.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // MySqlStatementLabel
+            // 
+            this.MySqlStatementLabel.AutoSize = true;
+            this.MySqlStatementLabel.Location = new System.Drawing.Point(8, 56);
+            this.MySqlStatementLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.MySqlStatementLabel.Name = "MySqlStatementLabel";
+            this.MySqlStatementLabel.Size = new System.Drawing.Size(87, 13);
+            this.MySqlStatementLabel.TabIndex = 2;
+            this.MySqlStatementLabel.Text = "MySql Statement";
+            // 
+            // ExecuteButton
+            // 
+            this.ExecuteButton.Location = new System.Drawing.Point(254, 80);
+            this.ExecuteButton.Margin = new System.Windows.Forms.Padding(2);
+            this.ExecuteButton.Name = "ExecuteButton";
+            this.ExecuteButton.Size = new System.Drawing.Size(71, 23);
+            this.ExecuteButton.TabIndex = 3;
+            this.ExecuteButton.Text = "Execute";
+            this.ExecuteButton.UseVisualStyleBackColor = true;
+            this.ExecuteButton.Click += new System.EventHandler(this.ExecuteButton_Click);
+            // 
+            // Result
+            // 
+            this.Result.FormattingEnabled = true;
+            this.Result.Location = new System.Drawing.Point(21, 129);
+            this.Result.Margin = new System.Windows.Forms.Padding(2);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(305, 160);
+            this.Result.TabIndex = 4;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(22, 296);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(2);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(50, 23);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(276, 296);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(2);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(50, 23);
+            this.closeButton.TabIndex = 6;
+            this.closeButton.Text = "CLOSE";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // departmentLabel
+            // 
+            this.departmentLabel.AutoSize = true;
+            this.departmentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.departmentLabel.Location = new System.Drawing.Point(93, 12);
+            this.departmentLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.departmentLabel.Name = "departmentLabel";
+            this.departmentLabel.Size = new System.Drawing.Size(180, 20);
+            this.departmentLabel.TabIndex = 7;
+            this.departmentLabel.Text = "DEPARTMENT DATA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 325);
+            this.Controls.Add(this.departmentLabel);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.ExecuteButton);
+            this.Controls.Add(this.MySqlStatementLabel);
+            this.Controls.Add(this.MySqlStatementTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MySqlStatementTextBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label MySqlStatementLabel;
+        private System.Windows.Forms.Button ExecuteButton;
+        private System.Windows.Forms.ListBox Result;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label departmentLabel;
+    }
+}
+
---
+++ Sql app_C#_windowsForm/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Assign3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // MySqlConnection to declare the server address, id, password, database
+                using (MySqlConnection Connection = new MySqlConnection("server=10.158.56.53;uid=csci473g58;pwd=wordpass58;database=csci473g58;"))
+                {
+                    Connection.Open();          // Opens the connection to perfrom MySql operations
+        
+                    MySqlCommand DropClassCommand = new MySqlCommand("DROP TABLE IF EXISTS ClassTable", Connection);
+                    DropClassCommand.ExecuteNonQuery();
+                    MySqlCommand DropRoomCommand = new MySqlCommand("DROP TABLE IF EXISTS RoomTable", Connection);
+                    DropRoomCommand.ExecuteNonQuery();
+                    MySqlCommand DropOfficeCommand = new MySqlCommand("DROP TABLE IF EXISTS OfficeTable", Connection);
+                    DropOfficeCommand.ExecuteNonQuery();
+                    MySqlCommand CreateRoomCommand = new MySqlCommand("CREATE TABLE RoomTable(Room INT, Capacity INT, Smart CHAR(1), PRIMARY KEY (Room))", Connection);
+                    CreateRoomCommand.ExecuteNonQuery();
+                    MySqlCommand CreateOfficeCommand = new MySqlCommand("CREATE TABLE OfficeTable(Teacher CHAR(20), Office INT, PRIMARY KEY (Teacher))", Connection);
+                    CreateOfficeCommand.ExecuteNonQuery();
+                    MySqlCommand CreateClassCommand = new MySqlCommand("CREATE TABLE ClassTable(Class INT, Section INT, Teacher CHAR(20), Room INT, Time CHAR(5), Days CHAR(5), Enrollment INT, FOREIGN KEY (Teacher) REFERENCES OfficeTable(Teacher), FOREIGN KEY (Room) REFERENCES RoomTable(Room))", Connection);
+                    CreateClassCommand.ExecuteNonQuery();
+                    String Path = Directory.GetCurrentDirectory();
+                    //Reads values from the file
+                    using (StreamReader SR = new StreamReader(Path + "\\Room.txt"))         // StreamReader to read values from file
+                    {
+                        String S = SR.ReadLine();
+                        while (S != null)
+                        {
+                            String[] sub = S.Split(',');            
+                            int Room = Convert.ToInt32(sub[0]);     
+                            int Capacity = Convert.ToInt32(sub[1]); 
+                            MySqlCommand InsertRoomCommand = new MySqlCommand("INSERT INTO RoomTable (Room, Capacity, Smart) VALUES (" + Room.ToString() + "," + Capacity.ToString() + ",'" + sub[2] + "')", Connection);
+                            InsertRoomCommand.ExecuteNonQuery();    // Execute the insert command
+                            S = SR.ReadLine();
+                        }
+                    } 
+                    using (StreamReader SR = new StreamReader(Path + "\\Office.txt"))        // StreamReader to read values from file
+                    {
+                        String S = SR.ReadLine();
+                        while (S != null)
+                        {
+                            String[] sub = S.Split(',');            
+                            int Office = Convert.ToInt32(sub[1]);   
+                            //MySqlCommand to insert values into OfficeTable
+                            MySqlCommand InsertOfficeCommand = new MySqlCommand("INSERT INTO OfficeTable (Teacher, Office) VALUES ('" + sub[0] + "'," + Office.ToString() + ")", Connection);
+                            InsertOfficeCommand.ExecuteNonQuery(); 
+                            S = SR.ReadLine();
+                        }
+                    }
+                    using (StreamReader SR = new StreamReader(Path + "\\Class.txt"))        //StreamReader to read values from file
+                    {
+                        String S = SR.ReadLine();
+                        while (S != null)
+                        {
+                            String[] sub = S.Split(',');           
+                            string subtext = sub[0];
+                            String[] text = subtext.Split('-');
+                            int Class = Convert.ToInt32(text[0]);    
+                            int Section = Convert.ToInt32(text[1]);  
+                            int Enrollment = Convert.ToInt32(sub[5]);   
+                            if (sub[2] == "null")
+                            {
+                                MySqlCommand InsertClassCommand = new MySqlCommand("INSERT INTO ClassTable (Class, Section, Teacher, Room, Time, Days, Enrollment) VALUES (" + Class.ToString() + "," + Section.ToString() + ",'" + sub[1] + "'," + sub[2] + ",'" + sub[3] + "','" + sub[4] + "'," + Enrollment.ToString() + ")", Connection);
+                                InsertClassCommand.ExecuteNonQuery();   
+                            }
+                            else
+                            {
+                                int Room = Convert.ToInt32(sub[2]);     
+                                MySqlCommand InsertClassCommand = new MySqlCommand("INSERT INTO ClassTable (Class, Section, Teacher, Room, Time, Days, Enrollment) VALUES (" + Class.ToString() + "," + Section.ToString() + ",'" + sub[1] + "'," + Room.ToString() + ",'" + sub[3] + "','" + sub[4] + "'," + Enrollment.ToString() + ")", Connection);
+                                InsertClassCommand.ExecuteNonQuery();   
+                            }
+                            S = SR.ReadLine();
+                        }
+                    }
+
+
+                    Connection.Close();         // Closes the connection
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connection is not established \n" + ex);     // Shows the exception if the connection is not established
+                Application.Exit();
+            }
+        }
+
+        private void ExecuteButton_Click(object sender, EventArgs e)
+        {
+            Result.Items.Clear();
+            try
+            {
+                // MySqlConnection to declare the server address, id, password, database
+                using (MySqlConnection Connection = new MySqlConnection("server=10.158.56.53;uid=csci473g58;pwd=wordpass58;database=csci473g58;"))
+                {
+                    Connection.Open();              // Opens the connection to perfrom MySql operations
+                    MySqlCommand QueryCommand = new MySqlCommand(MySqlStatementTextBox.Text, Connection);
+                    string query = MySqlStatementTextBox.Text.Substring(0, 6);       // Gets the first 6 letters of the query text box
+                    // If the query is select command
+                    if (query.ToLower() == "select")
+                    {
+                        try
+                        {
+                            MySqlDataReader Reader = QueryCommand.ExecuteReader();      // MySqlReader to execute the select command
+                            WriteColumn((IDataRecord)Reader);            // ReadColumn() to add the column names to the Result
+                            while (Reader.Read())
+                            {
+                                WriteSingleRow((IDataRecord)Reader);         // Calls the ReadSingleRow() to add the result to the Result
+                            }
+                            Reader.Close();
+                            MySqlStatementTextBox.Clear();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error in query entered \n" + ex);      // When the query is not executed due to error, exception is shown
+                            MySqlStatementTextBox.Clear();
+                        }
+                    }
+                    // If the query is insert command
+                    else if (query.ToLower() == "insert")
+                    {
+                        try
+                        {
+                            QueryCommand.ExecuteNonQuery();         // Executes the insert query
+                            Result.Items.Add("New record is added");
+                            MySqlStatementTextBox.Clear();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Record is not inserted \n" + ex);      // When the query is not executed due to error, exception is shown
+                            MySqlStatementTextBox.Clear();
+                        }
+                    }
+                    // If the query is something else
+                    else
+                    {
+                        Result.Items.Clear();
+                        Result.Items.Add("The entered MySql command is not yet implemented");
+                        MySqlStatementTextBox.Clear();
+                    }
+                    Connection.Close();         // Closes the connection
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connection is not established \n" + ex);       // Shows the exception if the connection is not established
+                Application.Exit();
+            }
+        }
+        // WriteColumn() to add the column names to Result
+        private void WriteColumn(IDataRecord reader)
+        {
+            string columns = null, lines = null;
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                columns += reader.GetName(i).PadRight(8) + "| ";      // Add the names of columns to string columns
+                lines += "----------";                              // Add lines after column names
+            }
+            Result.Items.Add(columns);
+            Result.Items.Add(lines);
+        }
+        // WriteSingleRow() to read the result of the query and add the result to Result
+        private void WriteSingleRow(IDataRecord reader)
+        {
+            string result = null;
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                result += reader[i].ToString().PadRight(8) + "| ";   // Add the result values to string result
+            }
+            Result.Items.Add(result);
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            MySqlStatementTextBox.Clear();
+            Result.Items.Clear();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+
+
+
+
+}
+
